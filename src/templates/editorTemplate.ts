854c846e665cd889f7e2a464b0d67939f05904c3
Nick Shockey <shockeynj@gmail.com>
2023-01-28 21:26:35 -0500

    Added up to git decorations in editor template

--- src/templates/editorTemplate.ts
+++ src/templates/editorTemplate.ts
@@ -106,6 +106,83 @@ export function createEditorTemplate(theme: Themes) {
 			"activityBar.background": editorColors.bgSideBar,
 			"activityBar.foreground": editorColors.fg,
 
+			// Status bar
+			"statusBar.background": editorColors.bgStatusBar,
+			"statusBar.foreground": editorColors.fg,
+			"statusBar.focusBorder": editorColors.transparent,
+			"statusBar.noFolderBackground": editorColors.bgStatusBar,
+			"statusBar.noFolderForeground": editorColors.fg,
+			"statusBar.debuggingBackground": "#004B80",
+			"statusBar.debuggingForeground": editorColors.fgSecondary,
+			"statusBarItem.remoteBackground": editorColors.fgDisabled,
+			"statusBarItem.remoteForeground": editorColors.fgSecondary,
+			"statusBarItem.hoverBackground": editorColors.selectionNormal,
+			"statusBarItem.activeBackground": editorColors.selectionBright,
+			"statusBarItem.focusBorder": editorColors.transparent,
+
+			// Menu
+			"menu.background": editorColors.bgEditor,
+			"menu.foreground": editorColors.fg,
+			"menu.selectionBackground": editorColors.selectionNormal,
+			"menu.selectionForeground": editorColors.fgSecondary,
+			"menu.separatorBackground": editorColors.fgDisabled,
+
+			// Title bar
+			"titleBar.activeBackground": editorColors.bgSideBar,
+			"titleBar.inactiveBackground": editorColors.bgStatusBar,
+			"titleBar.activeForeground": editorColors.fg,
+			"titleBar.inactiveForeground": editorColors.fgDisabled,
+
+			// Tabs
+			"tab.inactiveBackground": editorColors.bgStatusBar,
+			"tab.inactiveForeground": editorColors.fgDisabled,
+			"tab.activeForeground": editorColors.fgSecondary,
+			"tab.activeModifiedBorder": "#0086E6",
+			"tab.inactiveModifiedBorder": "#0086E6",
+
+			// Scrollbar slider
+			"scrollbarSlider.background":
+				editorColors.selectionBright +
+				editorColors.selectionTransparency,
+			"scrollbarSlider.hoverBackground":
+				"#004b80" + editorColors.selectionTransparency,
+			"scrollbarSlider.activeBackground":
+				editorColors.modifiedGutter +
+				editorColors.selectionTransparency,
+
+			// Minimap
+			"minimap.warningHighlight": editorColors.warning,
+			"minimap.errorHighlight": editorColors.error,
+			"minimapGutter.addedBackground": editorColors.added,
+			"minimapGutter.deletedBackground": editorColors.deleted,
+			"minimapGutter.modifiedBackground": editorColors.modified,
+
+			// Lists
+			"list.hoverBackground": editorColors.bgStatusBar,
+			"list.dropBackground":
+				editorColors.selectionBright +
+				editorColors.selectionTransparency,
+			"list.inactiveSelectionBackground": editorColors.bgEditor,
+			"list.activeSelectionForeground": editorColors.fgSecondary,
+			"list.inactiveFocusOutline": editorColors.bgEditor,
+			"list.focusOutline": editorColors.bgEditor,
+			"list.warningForeground": editorColors.warning,
+			"list.errorForeground": editorColors.error,
+			"tree.indentGuidesStroke": "#004B80",
+
+			// Git decorations
+			"gitDecoration.untrackedResourceForeground": editorColors.added,
+			"gitDecoration.modifiedResourceForeground": editorColors.modified,
+			"gitDecoration.renamedResourceForeground": editorColors.modified,
+			"gitDecoration.deletedResourceForeground": editorColors.deleted,
+			"gitDecoration.conflictingResourceForeground": editorColors.deleted,
+			"gitDecoration.stageDeletedResourceForeground":
+				editorColors.deleted,
+			"gitDecoration.ignoredResourceForeground": editorColors.ignored,
+			"gitDecoration.addedResourceForeground": editorColors.staged,
+			"gitDecoration.stageModifiedResourceForeground":
+				editorColors.staged,
+
 			// Bracket highlight
 			"editorBracketHighlight.foreground1": syntax.bracket1,
 			"editorBracketHighlight.foreground2": syntax.bracket2,
