ed80bd543461532c3c26bb3c8bd4fa3cd1f0e408
Nick Shockey <shockeynj@gmail.com>
2023-01-29 11:35:17 -0500

    Themes can now be fully generated from typescript
    Now I need to look at the changes made
    to make sure the themes are still basically the same

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,39 @@ import { createFullTemplate } from "./templates/fullTemplate";
 import { Themes } from "./colors";
 
 function createJson() {
-	const output = createFullTemplate(Themes.AllBlue);
+	const themes: Themes[] = [Themes.AllBlue, Themes.Calm, Themes.Waves];
 	const outputDir = "./themes";
 
 	if (!existsSync(outputDir)) {
 		mkdirSync(outputDir, { recursive: true });
 	}
 
-	writeFile(outputDir + "/test-color-theme.json", output, (err) => {
-		if (!err) {
-			console.log("Successfully wrote JSON to file.");
-		} else {
-			console.log(err);
+	for (let i = 0; i < themes.length; i++) {
+		const output = createFullTemplate(themes[i]);
+		let fileName = "";
+
+		switch (themes[i]) {
+			case Themes.AllBlue:
+				fileName = "all-blue";
+				break;
+			case Themes.Calm:
+				fileName = "all-blue-calm";
+				break;
+			case Themes.Waves:
+				fileName = "all-blue-waves";
+				break;
 		}
-	});
+
+		fileName += "-color-theme.json";
+
+		writeFile(outputDir + "/" + fileName, output, (err) => {
+			if (!err) {
+				console.log("Successfully wrote JSON files.");
+			} else {
+				console.log(err);
+			}
+		});
+	}
 }
 
 createJson();
